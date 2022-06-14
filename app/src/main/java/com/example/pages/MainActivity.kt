def4cee41be4877bd64db72df7dccd3c34cff6e8
nabzi <zeinab.modir@gmail.com>
2022-06-14 19:37:53 +0430

    Add fusedLocation client and get current / last location

--- app/src/main/java/com/example/pages/MainActivity.kt
+++ app/src/main/java/com/example/pages/MainActivity.kt
@@ -1,11 +1,94 @@
 package com.example.pages
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getLocationPermission()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun getLocationPermission() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    showLocation()
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    showLocation()
+                }
+                else -> {
+                    Toast.makeText(this, "goodbye", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    private fun showLocation() {
+        Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        if(!isLocationEnabled()){
+            Toast.makeText(this, "turn on your location", Toast.LENGTH_SHORT).show()
+            Log.d("location", "turn on your location")
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+                location?.let{
+                    Toast.makeText(this, "latitude" + it.latitude + " , long=" + it.longitude, Toast.LENGTH_LONG).show()
+                    Log.d("location lastLocation" , "latitude" + it.latitude + " , long=" + it.longitude)
+                }
+        }
+        fusedLocationClient.getCurrentLocation(	PRIORITY_HIGH_ACCURACY , null).addOnSuccessListener{
+            location : Location? ->
+            location?.let{
+                Toast.makeText(this, "latitude" + it.latitude + " , long=" + it.longitude, Toast.LENGTH_LONG).show()
+                Log.d("location currentLocation" , "latitude" + it.latitude + " , long=" + it.longitude)
+            }
+        }
+    }
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(locationManager)
     }
 }
+/*
+*
+* */
---
+++ app/src/main/java/com/example/pages/MainActivity2.kt
@@ -0,0 +1,54 @@
+package com.example.pages
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import me.relex.circleindicator.CircleIndicator3
+
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.pager)
+
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(this )
+        viewPager.adapter = pagerAdapter
+
+        val indicator: CircleIndicator3 = findViewById(R.id.indicator)
+        indicator.setViewPager(viewPager)
+
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = 2
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> AddFragment()
+                1 -> SearchFragment()
+                else -> AddFragment()
+            }
+
+        }
+    }
+}
